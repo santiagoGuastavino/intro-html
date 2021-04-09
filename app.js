8711e9908a011927145e0abe5d8ed3669109ce02
santiagoGuastavino <san.guasta@gmail.com>
2021-04-09 17:35:58 -0300

    first commit. html practice

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const { homedir } = require('os');
+const path = require('path');
+const app = express();
+const publicPath = path.resolve('./public');
+
+app.use(express.static(publicPath));
+app.listen(3000, () => console.log('Server running'));
+
+const homePath = path.resolve('./views/index.html');
+app.get('/', (req,res) => res.sendFile(homePath));
