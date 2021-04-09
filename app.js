2082e7724a7c3832c560833a146d88608eae48ab
santiagoGuastavino <san.guasta@gmail.com>
2021-04-09 18:43:09 -0300

    minor changes

--- app.js
+++ app.js
@@ -7,5 +7,5 @@ const publicPath = path.resolve('./public');
 app.use(express.static(publicPath));
 app.listen(3000, () => console.log('Server running'));
 
-const homePath = path.resolve('./views/index.html');
+const homePath = path.resolve('./views/home.html');
 app.get('/', (req,res) => res.sendFile(homePath));
